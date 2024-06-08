62f85fac329a2bbe109fb293bae7c29c2028c38c
MaganyAlves <magany.delima@edu.unifl.br>
2024-06-08 17:24:46 -0300

    delete Controller.js

--- src/pages/Lancados.js
+++ src/pages/Lancados.js
@@ -1,13 +1,46 @@
-import React from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, Button, FlatList } from "react-native";
 
 export default function Lancados({ navigation}) {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios("http://192.168.15.154:8081/lancamentos");
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderItem = ({item}) => {
+    <View>
+      <Text>{item.valor}</Text>
+      <Text>{item.descricao}</Text>
+      <Text>{item.categoria}</Text>
+      <Text>{item.data}</Text>
+      <Text>{item.tipo}</Text>
+      <Button title="Detalhes" onPress={() => navigation.navigate('Detalhes', {id: item.id})} />
+    </View>   
+  }
+
   return (
     <View style={styles.container}>
-      <Button title="Registrar" onPress={() => navigation.navigate('Registrar')} />
+      <Button title="Buscar" onPress={fetchData} />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
--- src/server/Service.js
+++ src/server/Service.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const  Api = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://192.168.15.154:8081',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 });
